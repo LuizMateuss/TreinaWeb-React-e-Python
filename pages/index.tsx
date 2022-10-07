679cebe204b3642cccc7b0301a2cc651d9f24e15
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-07 20:46:43 -0300

    configura componente de forma global para o site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next'
-import Cabecalho from '../src/components/Cabecalho/Cabecalho'
 
 const Home: NextPage = () => {
   return (
-   <Cabecalho></Cabecalho>
-  )
+      <div>oi</div>
+    )
 }
 
 export default Home
