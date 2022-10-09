4dc01ab6f6dddc96b4aaf86b59f5546a549d342f
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-09 16:57:20 -0300

    conecta app teacher com o projeto, cria migration

--- backend/teacher/models.py
+++ backend/teacher/models.py
@@ -1,3 +1,19 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    value_hour = models.DecimalField(max_digits=9, decimal_places=2, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    picture = models.URLField(max_length=255, null=False, blank=False)
+
+
+# >python manage.py makemigrations
+# >python manage.py migrate
+
+# >>> from teacher.models import Teacher
+# >>> teacher = Teacher(name="Luiz Mateus", value_hour=96.60, description="Professor de música e violão", picture="https://github.com/luizmateuss.png")
+# >>> teacher.nome
+# >>> teacher.name 
+# 'Luiz Mateus'
+# >>> teacher.save()
