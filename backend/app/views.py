72b081c6aafb55b08faa5424302340da2c58126f
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-09 16:28:01 -0300

    configura app com django rest

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 
-def home(request):
-    # return render(request, 'app/home.html')
-    return JsonResponse({"nome": "Luiz Mateus", "idade": 20})
+# def home(request):
+#     # return render(request, 'app/home.html')
+#     return JsonResponse({"nome": "Luiz Mateus", "idade": 20})
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return Response({"nome": "Luiz Mateus", "idade": 20}, status=200)
--- backend/myteacher/urls.py
+++ backend/myteacher/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app.views import home
+# from app.views import home
+from app.views import HomeApiView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    # path('', home)
+    path('', HomeApiView.as_view())
 ]
