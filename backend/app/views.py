385b7d2c8bdb80d960305f210bcf16c77105ded8
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-09 14:54:02 -0300

    adiciona app ao projeto django, configura modelo MTV para explicação

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    # return render(request, 'app/home.html')
+    return JsonResponse({"nome": "Luiz Mateus", "idade": 20})
