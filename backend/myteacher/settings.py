1134653193e99f16ccbd48eab0a358ede8efb45f
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-10 01:33:25 -0300

    configura arquivo para gerenciar pacotes pip

--- backend/myteacher/settings.py
+++ backend/myteacher/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from myconfig import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%!1%3sgi413ti6bxkgm0qsrprc#mv(f3@5sl=s$uds3se6y$qb'
+SECRET_KEY = Config.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
