eabc0c94363bff0c2997ef534006d2f11e438200
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-12 13:19:54 -0300

    instala axios e configura serviço para puxar dados do backend

--- frontend/src/hooks/pages/useIndex.ts
+++ frontend/src/hooks/pages/useIndex.ts
@@ -1,37 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Teacher } from "../../@types/teacher";
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex(){
-    const [listTeachers, setListTeachers] = useState<Teacher[]>([
-        {
-            id: 1,
-            name: "Luiz Mateus",
-            picture: "https://github.com/luizmateuss.png",
-            description: "Aula de música e violão",
-            value_hour: 95.36
-        },
-        {
-            id: 2,
-            name: "Professor 2",
-            picture: "https://github.com/luizmateuss.png",
-            description: "Descrição professor 2",
-            value_hour: 200
-        },
-        {
-            id: 3,
-            name: "Professor 3",
-            picture: "https://github.com/luizmateuss.png",
-            description: "Descrição professor 3",
-            value_hour: 300
-        },
-        {
-            id: 2,
-            name: "Professor 2",
-            picture: "https://github.com/luizmateuss.png",
-            description: "Descrição professor 2",
-            value_hour: 200
-        }
-    ])
+    const [listTeachers, setListTeachers] = useState<Teacher[]>([])
+
+    useEffect(() => {
+        ApiService.get('/teachers').then((response)=>{
+            setListTeachers(response.data)
+        })
+    }, [])
     return{
         listTeachers
     }
