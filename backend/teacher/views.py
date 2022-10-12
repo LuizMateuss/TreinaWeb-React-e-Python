1e66ee5a116fa4932073d8de1a19a350a262789b
LuizMateuss <luiz.alonso@etec.sp.gov.br>
2022-10-12 12:53:42 -0300

    corrige erro de digitação na configuração do cors do django e na importação da Response no rest

--- backend/teacher/views.py
+++ backend/teacher/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
-from rest_framework.views import Response
+from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from teacher.models import Teacher, Class
